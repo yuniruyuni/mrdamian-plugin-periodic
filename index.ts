84103fc5ef3ca2628c6548b8790748f8ba5de1b2
yuniruyuni <yuniruyuni@gmail.com>
2024-06-25 09:38:36 +0900

    refactor: migrate component from base repo

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { setTimeout } from "node:timers/promises";
+import { Component } from "mrdamian/model/component";
+import type { ComponentConfig } from "mrdamian/model/parameters";
+import type { Field } from "mrdamian/model/variable";
+
+type PeriodicConfig = ComponentConfig & {
+  interval: number;
+};
+
+export class Periodic extends Component<PeriodicConfig> {
+  async init(config: PeriodicConfig): Promise<Field> {
+    this.start(config);
+    return undefined;
+  }
+  async run(): Promise<Field> {
+    return undefined;
+  }
+  async start(config: PeriodicConfig): Promise<void> {
+    for (;;) {
+      await setTimeout(config.interval);
+      this.emit(true);
+    }
+  }
+}
