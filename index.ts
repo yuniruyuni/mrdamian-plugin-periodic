fad91280b481068388c9c36436fe2492c7484630
yuniruyuni <yuniruyuni@gmail.com>
2024-06-27 03:05:16 +0900

    feat: update plugin interface

--- index.ts
+++ index.ts
@@ -4,21 +4,20 @@ import type { ComponentConfig } from "mrdamian/model/parameters";
 import type { Field } from "mrdamian/model/variable";
 
 type PeriodicConfig = ComponentConfig & {
-  interval: number;
+	interval: number;
 };
 
 export default class Periodic extends Component<PeriodicConfig> {
-  async init(config: PeriodicConfig): Promise<Field> {
-    this.start(config);
-    return undefined;
-  }
-  async run(): Promise<Field> {
-    return undefined;
-  }
-  async start(config: PeriodicConfig): Promise<void> {
-    for (;;) {
-      await setTimeout(config.interval);
-      this.emit(true);
-    }
-  }
+	async initialize(config: PeriodicConfig): Promise<void> {
+		this.start(config);
+	}
+	async process(): Promise<Field> {
+		return undefined;
+	}
+	async start(config: PeriodicConfig): Promise<void> {
+		for (;;) {
+			await setTimeout(config.interval);
+			this.emit(true);
+		}
+	}
 }
