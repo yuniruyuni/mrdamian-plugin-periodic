5cc4429c0fe5d608e6b7ab0dc4b4220f2a9f8b10
yuniruyuni <yuniruyuni@gmail.com>
2024-07-24 12:52:01 +0900

    feat: update plugin interface

--- index.ts
+++ index.ts
@@ -1,21 +1,32 @@
 import { setTimeout } from "node:timers/promises";
-import { Component, type ComponentConfig, type Field } from "mrdamian-plugin";
+import type { Component, ComponentConfig, EventEmitter } from "mrdamian-plugin";
 
 type PeriodicConfig = ComponentConfig & {
 	interval: number;
 };
 
-export default class Periodic extends Component<PeriodicConfig> {
-	async initialize(config: PeriodicConfig): Promise<void> {
-		this.start(config);
-	}
-	async process(): Promise<Field> {
-		return undefined;
-	}
-	async start(config: PeriodicConfig): Promise<void> {
-		for (;;) {
+class Runner {
+	running = true;
+	stop(){ this.running = false; }
+	async run(config: PeriodicConfig, emitter: EventEmitter): Promise<void> {
+		while( this.running ) {
 			await setTimeout(config.interval);
-			this.emit(true);
+			emitter.emit(true);
 		}
+	}
+}
+
+export default class Periodic implements Component<PeriodicConfig> {
+	runner: Runner = new Runner();
+
+	async start(config: PeriodicConfig, emitter: EventEmitter): Promise<void> {
+		this.runner.stop();
+		this.runner = new Runner();
+		this.runner.run(config, emitter);
+	}
+
+	async stop(): Promise<void> {
+		this.runner.stop();
+		this.runner = new Runner();
 	}
 }
